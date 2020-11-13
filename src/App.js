b474a183630b2c80991d0253aa611a410784e8bd
Dylan Crawshaw <dcrawsh@gmail.com>
2020-11-12 19:28:03 -0800

    add SearchForm.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/SearchForm';
+import SearchForm from './components/SearchForm';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <h1>Who Plays It???</h1>
+      <SearchForm/>
     </div>
   );
 }
