67ffd5f7e5b304397f1abb628e65933d83f2fb3c
Dylan Crawshaw <dcrawsh@gmail.com>
2020-11-24 14:43:10 -0800

    bug fix

--- src/components/GetLyrics.js
+++ src/components/GetLyrics.js
@@ -1,23 +1,15 @@
 import React from 'react'
-import scraper from "azlyrics-scraper";
-
-
 
 
 export default function GetLyrics(props) {
 
     
 
-    let song = props.songName.replaceAll(' ', '').toLowerCase()
-    let artist = props.artistName.replaceAll(' ', '').toLowerCase()
+    
 
     
     const handleClick = (e) => { 
-        return scraper.getLyricFromLink(`https://www.azlyrics.com/lyrics/${artist}/${song}.html`).then(result => {
-          props.setLyrics(result.join(" "));
-      }).catch(error => {
-          console.log(error)
-      });
+        
       }
     
     
