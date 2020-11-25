763af03dd13c8b0f6030c0579d90edc1c9ffeda7
Dylan Crawshaw <dcrawsh@gmail.com>
2020-11-25 11:28:17 -0800

    update SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SearchForm(props) {
     return (
         <form onSubmit={props.handleSubmit}>
-            <input type="text" name="searchInput" value={props.searchInput} onChange={props.handleInput}/>
+            <input placeholder="Search a Song Name" type="text" name="searchInput" value={props.searchInput} onChange={props.handleInput}/>
             <input type="submit"></input>
             
         </form>
