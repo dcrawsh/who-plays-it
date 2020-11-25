5d274e06caa046fc54b6d21873b496627e130f11
Dylan Crawshaw <dcrawsh@gmail.com>
2020-11-24 20:34:19 -0800

    change Lyrics.js

--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Lyric(props) {
 
     return (
-        <div dangerouslySetInnerHTML={{__html: props.lyrics}} className='danger-zone' >
+        <div dangerouslySetInnerHTML={{__html: props.lyrics}} className='display-lyrics' >
         
         </div>
     )
